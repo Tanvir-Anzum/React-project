c1354780f2b3c57dcf8c68020e232e7e731e3f19
Tanvir Anzum <sshihab133075@gmail.com>
2021-10-04 17:44:18 +0600

    paths defined

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import logo from './logo.svg'
 import './App.css'
 import { useEffect, useState } from 'react'
 import Home from './components/Home/Home'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import NOtFound from './components/NotFound/NOtFound'
 
 function App() {
 
   return (
-    <Home></Home>
+     <BrowserRouter>
+       <Switch>
+         <Route path="/Home">
+           <Home></Home>
+         </Route>
+         <Route>
+           <NOtFound></NOtFound>
+         </Route>
+       </Switch>
+       </BrowserRouter>
+  
   )
 }
 
--- src/components/NotFound/NOtFound.js
+++ src/components/NotFound/NOtFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NOtFound = () => {
  return (
   <div>
-   
+   <h1>Page Not Found</h1>
   </div>
  );
 };
