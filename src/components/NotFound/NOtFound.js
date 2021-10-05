bf6a02bc9431f136402e0dc37891ab5bdbc7dff1
Tanvir Anzum <sshihab133075@gmail.com>
2021-10-05 21:57:56 +0600

    completed

--- src/components/NotFound/NOtFound.js
+++ src/components/NotFound/NOtFound.js
@@ -5,7 +5,9 @@ const NOtFound = () => {
     // This component defines what will be donwe if the path doesnt match with a=the defined routes of "App.js"
     <div className='d-flex justify-content-center NotFound'>
       <div>
-        <h1>404</h1>
+        <div className='d-flex justify-content-center'>
+          <h1>404</h1>
+        </div>
         <h1>Page Not Found</h1>
       </div>
     </div>
